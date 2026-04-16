6825cba40c14237ef4c6bd84fb0be6ed4666b492
puranrana080 <puransinghrana080@gmail.com>
2026-04-16 16:54:55 +0000

    v12 first commit

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("com.demolscds.p5felrcds.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
